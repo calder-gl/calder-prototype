9af065eab8e217a0e6156ed700a32c8206d4d9e5
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-10-20 13:52:54 -0400

    Updated gulpfile, and README documentation.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
+# calder-gl
+:art: Written by &mdash; Paul Bardea, Abhishek Madan, Andrew McBurney, Dave Pagurek Van Mossel
+___
+
+### Development Setup
 ```bash
+# Install dependencies
 yarn install
+
+# Watch for changes to TypeScript, and build on change
 gulp watch
+
+# Run the compiled es5 JavaScript code
 npm start
 ```
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,23 @@
-import gulp from "gulp";
-import babel from "gulp-babel";
+//
+// gulpfile.babel.js
+// calder-gl
+//
+
+import babel      from "gulp-babel";
+import gulp       from "gulp";
+import merge      from "merge2";
 import typescript from "gulp-typescript";
-import merge from "merge2";
 
 const src = "./src/**/*.ts";
 const out = "./build";
 const babelConf = { presets: ["es2015"] };
+
 const project = typescript.createProject("tsconfig.json", {
   outDir: out,
-  typescript: require("typescript"),
+  typescript: require("typescript")
 });
 
+// Gulp task to build .ts to es5 using babel
 gulp.task("build", function () {
   var result = gulp.src(src).pipe(typescript(project));
 
@@ -22,8 +29,10 @@ gulp.task("build", function () {
   ]);
 });
 
+// Gulp task to watch for changes to .ts files, and build on change
 gulp.task("watch", ["build"], function () {
   gulp.watch(src, ["build"]);
 });
 
+// Set default gulp task as 'build' task
 gulp.task("default", ["build"]);
