8335e589ebc477562a8cc291e4b607122d9dd4c6
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-11-12 18:49:41 -0500

    Cleaned up function.spec.js

--- test/function.spec.js
+++ test/function.spec.js
@@ -3,9 +3,15 @@ import * as cgl from '../src/calder';
 
 describe('Function', () => {
     describe('source', () => {
-        const glPosition = new cgl.InterfaceVariable(cgl.Qualifier.Out, new cgl.VariableSource(new cgl.Type(cgl.Kind.Vec4), 'gl_Position'));
-        const vertexPosition = new cgl.InterfaceVariable(cgl.Qualifier.Attribute, new cgl.VariableSource(new cgl.Type(cgl.Kind.Vec4), 'vertexPosition'));
-        const integerVar = new cgl.InterfaceVariable(cgl.Qualifier.In, new cgl.VariableSource(new cgl.Type(cgl.Kind.Int), '0'));
+        const glPosition = new cgl.InterfaceVariable(
+            cgl.Qualifier.Out, new cgl.VariableSource(new cgl.Type(cgl.Kind.Vec4), 'gl_Position')
+        );
+        const vertexPosition = new cgl.InterfaceVariable(
+            cgl.Qualifier.Attribute, new cgl.VariableSource(new cgl.Type(cgl.Kind.Vec4), 'vertexPosition')
+        );
+        const integerVar = new cgl.InterfaceVariable(
+            cgl.Qualifier.In, new cgl.VariableSource(new cgl.Type(cgl.Kind.Int), '0')
+        );
 
         it('creates the correct source', () => {
             const main = new cgl.Function('main', [
@@ -23,9 +29,7 @@ describe('Function', () => {
         });
 
         it('handles different return types', () => {
-            const test = new cgl.Function('test', [
-                new cgl.ReturnStatement(new cgl.Reference(integerVar)),
-            ], cgl.Kind.Int);
+            const test = new cgl.Function('test', [new cgl.ReturnStatement(new cgl.Reference(integerVar))], cgl.Kind.Int);
 
             expect(test.source()).to.equalIgnoreSpaces(`int test() {
                 return 0;
