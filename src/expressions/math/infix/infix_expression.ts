1de3f45ea8be6759f3fede9b454083dfed0e24dc
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-11-12 15:31:08 -0500

    Removed 'dependencies' from infix_expression.ts.

--- src/expressions/math/infix/infix_expression.ts
+++ src/expressions/math/infix/infix_expression.ts
@@ -18,10 +18,6 @@ export default abstract class InfixExpression implements Expression {
         this.rhs = rhs;
     }
 
-    public dependencies(): Set<InterfaceVariable> {
-        return this.rhs.dependencies().union(this.lhs.dependencies());
-    }
-
     public abstract source(): string;
 
     public returnType(): Type {
