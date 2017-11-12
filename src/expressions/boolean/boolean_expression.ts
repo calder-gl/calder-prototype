9cce079919597b7a3dfc263881d24b92bb85e9db
Abhishek Madan <abhi.madan01@gmail.com>
2017-11-11 19:56:06 -0500

    Fixed broken tests post-rebase.

--- src/expressions/boolean/boolean_expression.ts
+++ src/expressions/boolean/boolean_expression.ts
@@ -3,13 +3,14 @@ import InterfaceVariable from '../../interface';
 import Reference from '../../reference';
 import Set from '../../util/set';
 import Type from '../../type';
+import Kind from '../../kind';
 
 export default abstract class BooleanExpression implements Expression {
     protected lhs: Expression;
     protected rhs: Expression;
 
     constructor(lhs: Expression, rhs: Expression) {
-        if (lhs.returnType() != rhs.returnType())
+        if (!lhs.returnType().checkEquals(rhs.returnType()))
             throw new TypeError("Left-hand side and right-hand side do not match types.");
 
         this.lhs = lhs;
@@ -23,6 +24,6 @@ export default abstract class BooleanExpression implements Expression {
     public abstract source(): string;
 
     public returnType(): Type {
-        return Type.Bool;
+        return new Type(Kind.Bool);
     }
 }
